e620e618213a5da43be8b653998ea12c63b053af
tiffanyannhuey <87tiffanyhuey@gmail.com>
2020-05-15 11:52:10 -0700

    Replace pdf and updates to layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,37 @@
+const fadeIn = () => {
+  $('.fade-in').each( function () {
+
+      let top_of_object = $(this).position().top;
+      let bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      if (bottom_of_window > top_of_object) {
+
+          $(this).animate({
+              'opacity': '1'
+          }, 500);
+
+      }
+
+  });
+}
+
+fadeIn();
+
 if ($(window).innerWidth() > 991) { 
 
   if ($('html.no-touch')) {
 
     $(".work-wrapper").hover(function (){
            $(this).find(".work-title").slideToggle("slow");
-      });
+    });
+
   }
 }
 
 $('.work-wrapper').on('click', function(e){
   e.preventDefault();
-  console.log(this);
-  var text = $(this).find('.work-title').html();
-  console.log(text);
-  var url = $(this).find('.link').attr('href');
+  let text = $(this).find('.work-title').html();
+  let url = $(this).find('.link').attr('href');
   console.log("url is", url);
 
   $('.modal-body').load('' + url + ' .main-content',function(){
@@ -21,34 +39,30 @@ $('.work-wrapper').on('click', function(e){
     });
 })
 
+$(window).scroll( () => {
+  fadeIn();
+});
 
-function backToTop () {
+const backToTop = () => {
 
-    var chaser = $('#js-chaser')
-
-      windowHeight = $(window).height();
+    let chaser = $('#js-chaser');
+    let  windowHeight = $(window).height();
 
     if (chaser) {
 
       $(window).scroll(function() {
 
         if ($(window).scrollTop() >= windowHeight / 2) {
-
           chaser.fadeIn();
-
         }
 
         else {
-
           chaser.hide();
-
         };
 
       });
 
-
-
-      chaser.on('click', function () {
+      chaser.on('click', () => {
 
         $('html, body').animate({scrollTop: '0px'}, 800);
  
@@ -56,12 +70,10 @@ function backToTop () {
 
     };
 
-  };
+};
 
-  // if ($(window).innerWidth() >= 768) {
+backToTop();
 
-    var backToTop = new backToTop;
 
-  // };
 
 
